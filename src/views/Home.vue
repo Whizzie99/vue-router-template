0d5f85924a3071ffed7b924e987626fbc1d21204
Whizzie99 <47413415+Whizzie99@users.noreply.github.com>
2021-02-23 17:54:53 +0100

    initial commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis magnam qui a enim perferendis sit et, eius unde, velit adipisci nemo laudantium veritatis reprehenderit officia sapiente. Modi corporis, deserunt explicabo dignissimos voluptatem dolore nulla. At, nisi? Molestias alias est, minima labore perferendis molestiae sequi incidunt vitae sed distinctio tenetur qui nisi? Ut, voluptatem, eligendi labore itaque suscipit cumque asperiores obcaecati aut voluptatum provident consequatur quia perspiciatis debitis tenetur facilis distinctio quos culpa necessitatibus a delectus nisi reprehenderit rerum blanditiis? Officiis doloribus quos, sapiente explicabo eius ullam nostrum aliquam quia voluptates? Debitis hic suscipit quia quibusdam consequuntur, nobis sint atque perspiciatis!</p>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis magnam qui a enim perferendis sit et, eius unde, velit adipisci nemo laudantium veritatis reprehenderit officia sapiente. Modi corporis, deserunt explicabo dignissimos voluptatem dolore nulla. At, nisi? Molestias alias est, minima labore perferendis molestiae sequi incidunt vitae sed distinctio tenetur qui nisi? Ut, voluptatem, eligendi labore itaque suscipit cumque asperiores obcaecati aut voluptatum provident consequatur quia perspiciatis debitis tenetur facilis distinctio quos culpa necessitatibus a delectus nisi reprehenderit rerum blanditiis? Officiis doloribus quos, sapiente explicabo eius ullam nostrum aliquam quia voluptates? Debitis hic suscipit quia quibusdam consequuntur, nobis sint atque perspiciatis!</p>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis magnam qui a enim perferendis sit et, eius unde, velit adipisci nemo laudantium veritatis reprehenderit officia sapiente. Modi corporis, deserunt explicabo dignissimos voluptatem dolore nulla. At, nisi? Molestias alias est, minima labore perferendis molestiae sequi incidunt vitae sed distinctio tenetur qui nisi? Ut, voluptatem, eligendi labore itaque suscipit cumque asperiores obcaecati aut voluptatum provident consequatur quia perspiciatis debitis tenetur facilis distinctio quos culpa necessitatibus a delectus nisi reprehenderit rerum blanditiis? Officiis doloribus quos, sapiente explicabo eius ullam nostrum aliquam quia voluptates? Debitis hic suscipit quia quibusdam consequuntur, nobis sint atque perspiciatis!</p>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis magnam qui a enim perferendis sit et, eius unde, velit adipisci nemo laudantium veritatis reprehenderit officia sapiente. Modi corporis, deserunt explicabo dignissimos voluptatem dolore nulla. At, nisi? Molestias alias est, minima labore perferendis molestiae sequi incidunt vitae sed distinctio tenetur qui nisi? Ut, voluptatem, eligendi labore itaque suscipit cumque asperiores obcaecati aut voluptatum provident consequatur quia perspiciatis debitis tenetur facilis distinctio quos culpa necessitatibus a delectus nisi reprehenderit rerum blanditiis? Officiis doloribus quos, sapiente explicabo eius ullam nostrum aliquam quia voluptates? Debitis hic suscipit quia quibusdam consequuntur, nobis sint atque perspiciatis!</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
